aa763e1f22740723adab4f003ff59361233cacee
shandy5833 <shandy5833@naver.com>
2022-05-04 18:20:20 +0900

    finished

--- Ch.1/Introduction.py
+++ Ch.1/Introduction.py
@@ -13,7 +13,50 @@
 #     name: python3
 # ---
 
-# np.array : n개의 행, m개의 열을 가진 array를 생성
+# ## 1.1 왜 머신러닝인가?
+#
+# - 하드코딩된 규칙만으로는 구현할 수 없는 분야가 많음  
+#   ex. 얼굴 인식  
+#   -> __머신러닝 사용__
+#
+# ### 1.1.1 머신러닝으로 풀 수 있는 문제
+#
+# - 입력과 기대되는 출력이 제공되는 __지도 학습__  
+#   ex. 스팸 분류
+# - 입력은 주어지지만 출력은 제공되지 않는 __비지도 학습__  
+#   ex. 블로그 글의 주제 구분
+# - 머신러닝에서의 하나의 개체 혹은 데이터셋의 행을 __샘플__ 또는 __데이터 포인트__, 그리고 샘플의 속성 혹은 데이터셋의 열을 __특성__이라고 한다.
+#
+# ### 1.1.2 문제와 데이터 이해하기
+#
+# - 어떤 질문에 대한 답을 원하는가? 가지고 있는 데이터가 원하는 답을 줄 수 있는가?
+# - 내 질문을 머신러닝의 문제로 가장 잘 기술하는 방법은 무엇인가?
+# - 문제를 풀기에 충분한 데이터를 모았는가?
+# - 내가 추출한 데이터의 특성은 무엇이며 좋은 예측을 만들어낼 수 있을 것인가?
+# - 머신러닝 애플리케이션의 성과를 어떻게 측정할 수 있는가?
+# - 머신러닝 솔루션이 다른 연구나 제품과 어떻게 협력할 수 있는가?
+
+# ## 1.2 왜 파이썬인가?
+#
+# - 데이터 적재, 시각화, 통계, 자연어 처리, 이미지 처리 등에 필요한 라이브러리
+# - 터미널이나 주피터 노트북으로 대화형 모드를 사용할 수 있음  
+#   \- _대화형 모드가 스크립트 모드에 비해 갖는 장점이 뭘까?_
+
+# ## ~1.3 scikit-learn~
+# ## 1.4 필수 라이브러리와 도구들
+#
+# ### 1.4.1 주피터 노트북
+# - 프로그램 코드를 브라우저에서 실행해주는 대화식 환경
+# - 탐색적 데이터 분석에 적합  
+#   - 확증적 데이터 분석 : 가설을 설정한 후, 수집한 데이터로 가설을 평가하고 추정하는 전통적인 분석  
+#   가설 설정 -> 데이터 수집 -> 통계 분석 -> 가설 검증
+#   - 탐색적 데이터 분석 : Raw data를 가지고 유연하게 데이터를 탐색하고, 데이터의 특징과 구조로부터 얻은 정보를 바탕으로 통계모형을 만드는 분석  
+#   데이터 수집 -> 시각화 탐색 -> 패턴 도출 -> 인사이트 발견
+
+# ### 1.4.2 Numpy
+# - 고수준 수학 함수와 유사 난수 생성기를 포함  
+#
+# numpy.array : n개의 행, m개의 열을 가진 array를 생성
 
 # +
 import numpy as np
@@ -22,55 +65,93 @@ x = np.array([[1, 2, 3], [4, 5, 6]])
 print("x:\n", x)
 # -
 
+# ### 1.4.3 SciPy
+# - 고성능 선형 대수, 함수 최적화, 신호 처리, 특수한 수학 함수와 통계 분포 등을 포함  
+#
+# scipy.sparse : 희소 행렬 기능 제공  
+# 희소 행렬 : 0을 많이 포함한 2차원 배열을 저장할 때 사용  
+#   
 # 단위 행렬 : 대각선 원소는 1이고 나머지는 0인 행렬  
 # np.eye : 크기가 n인 단위 행렬을 생성
 
 # +
 from scipy import sparse
 
+# 대각선 원소는 1이고 나머지는 0인 2차원 NumPy 배열을 만듭니다.
 eye = np.eye(4)
 print("NumPy 배열:\n", eye)
 # -
 
-# 희소 행렬 : 행렬의 값이 대부분 0인 행렬  
+# 희소 행렬을 0이 모두 채워진 2차원 배열로부터 만들지 않음(메모리 문제)  
 # 희소 행렬의 표현 방식 : CSR 포맷, COO 포맷  
+# CSR 포맷과 COO 포맷의 차이 : https://bit.ly/3vVmAik  
 # sparse.csr_matrix : CSR 포맷의 희소 행렬로 변환
 
-#CSR 포맷
+# NumPy 배열을 CSR 포맷의 SciPy 희박 행렬로 변환합니다.
+# 0이 아닌 원소만 저장됩니다.
 sparse_matrix = sparse.csr_matrix(eye)
 print("\nSciPy의 CSR 행렬:\n", sparse_matrix)
 
-# np.arange : 반열린구간 [start, stop)에서 step의 크기만큼 일정하게 떨어져 있는 숫자들을 array 형태로 반환  
+# numpy.arange : 반열린구간 [start, stop)에서 step의 크기만큼 일정하게 떨어져 있는 숫자들을 array 형태로 반환  
+# numpy.ones : 1로 채워진 array를 생성  
 # sparse.coo_matrix :  COO 포맷의 희소 행렬로 변환
 
-#COO 포맷
 data = np.ones(4)
 row_indices = np.arange(4)
 col_indices = np.arange(4)
 eye_coo = sparse.coo_matrix((data, (row_indices, col_indices)))
 print("COO 표현:\n", eye_coo)
 
-# np.linspace : start와 stop 사이를 일정 간격으로 잘라 1차원 배열을 생성
+# ### 1.4.4 matplotlib
+# - 선 그래프, 히스토그램, 산점도 등  
+#
+# numpy.linspace : start와 stop 사이를 일정 간격으로 잘라 1차원 배열을 생성  
 # plt.plot : 한 배열의 값을 다른 배열에 대응해서 선 그래프를 도시
 
 # +
 # %matplotlib inline
 import matplotlib.pyplot as plt
 
+# -10에서 10까지 100개의 간격으로 나뉘어진 배열을 생성합니다.
 x = np.linspace(-10, 10, 100)
+# 사인 함수를 이용하여 y 배열을 생성합니다.
 y = np.sin(x)
+# plot 함수는 한 배열의 값을 다른 배열에 대응해서 선 그래프를 그립니다.
 plt.plot(x, y, marker="x")
+# -
+
+# ### 1.4.5 pandas
+# - 데이터 처리와 분석을 위한 파이썬 라이브러리
+# - SQL처럼 테이블에 쿼리나 조인을 수행할 수 있음.
 
 # +
 import pandas as pd
 
+# 회원 정보가 들어간 간단한 데이터셋을 생성합니다.
 data = {'Name' : ["John", "Anna", "Peter", "Linda"],
        'Location' : ["New York", "Paris", "Berlin", "London"],
        'Age' : [24, 13, 53, 33]
        }
 
 data_pandas = pd.DataFrame(data)
+# 주피터 노트북은 Dataframe을 미려하게 출력해줍니다.
 data_pandas
 # -
 
+# - 테이블에 질의할 수 있음
+
+# Age 열의 값이 30 이상인 모든 행을 선택합니다.
 data_pandas[data_pandas.Age > 30]
+
+# ### 1.4.6 mglearn
+# - 간단하게 그림을 그리거나 필요한 데이터를 바로 불러들이기 위해 사용  
+#
+# 모든 코드는 다음의 라이브러리를 임포트한다고 가정
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import mglearn
+
+# ## ~~1.5 파이썬 2 vs. 파이썬 3~~
+# ## ~~1.6 이 책에서 사용하는 소프트웨어 버전~~
--- Ch.1/Iris_Classification.py
+++ Ch.1/Iris_Classification.py
@@ -13,33 +13,88 @@
 #     name: python3
 # ---
 
+# ## 1.7 첫 번째 애플리케이션: 붓꽃의 품종 분류
+# - 3가지 종으로 분류한 붓꽃의 측정 데이터가 주어짐  
+#   이 측정값을 이용해서 임의의 붓꽃이 어떤 품종인지 구분  
+#
+#   
+# - 붓꽃의 품종을 정확하게 분류한 데이터를 가지고 있으므로 __지도 학습__에 속함  
+#   3가지 종 중 하나를 선택하는 문제이므로 __분류__ 문제에 해당  
+#   이 때 출력될 수 있는 값(붓꽃의 종류)들을 __클래스__라고 함
+#
+#   
+# - 데이터 포인트 하나에 대한 기대 출력 : 꽃의 품종  
+#   특정 데이터 포인트에 대한 출력, 즉 품종을 __레이블__이라고 함  
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import mglearn
 
+# ### 1.7.1 데이터 적재
+# - 머신러닝과 통계 분야에서 오래전부터 사용해온 붓꽃 데이터셋
+
 from sklearn.datasets import load_iris
 iris_dataset = load_iris()
 
+# - iris 객체는 파이썬의 딕셔너리와 유사한 Bunch 클래스의 객체 : 키와 값으로 구성
+
 print("iris_dataset의 키:\n", iris_dataset.keys())
+
+# - DESCR 키에는 데이터셋에 대한 간략한 설명이 들어 있음
 
 print(iris_dataset['DESCR'][:193] + "\n...")
 
-print("타깃의 이름:", iris_dataset['target_names'])
+# - target_names : 예측하려는 붓꽃 품종의 이름의 문자열 배열
+#
+#   
+# - feature_names : 각 특성을 설명하는 문자열 리스트
 
-print("특성의 이름?\n", iris_dataset['feature_names'])
+print("타깃의 이름:", iris_dataset['target_names'])
+print("특성의 이름:\n", iris_dataset['feature_names'])
+
+# - 실제 데이터는 target과 data 필드에 들어 있음
+# - data : 꽃잎의 길이와 폭, 꽃받침의 길이와 폭을 수치 값으로 가지고 있는 NumPy 배열
 
 print("data의 타입:", type(iris_dataset['data']))
+
+# - 머신러닝에서 각 아이템은 __샘플__이라 하고, 속성은 __특성__이라 부름  
+#   -> data 배열의 크기는 샘플의 수에 특성의 수를 곱한 값
+#
+#   
+# - scikit-learn은 항상 데이터가 이런 구조일 것이라 가정
 
 print("data의 크기:", iris_dataset['data'].shape)
 
 print("data의 처음 다섯 행:\n", iris_dataset['data'][:5])
+
+# - target : 샘플 붓꽃의 품종을 담은 NumPy 배열
 
 print("target의 타입:", type(iris_dataset['target']))
 
 print("target의 크기:", iris_dataset['target'].shape)
 
 print("타깃:\n", iris_dataset['target'])
+
+# ### 1.7.2 성과 측정: 훈련 데이터와 텍스트 데이터
+# - 모델을 데이터에 적용하기 전에, 모델의 예측을 신뢰할 수 있는지 알아야 함  
+#
+#   
+# - 모델을 만들 때 쓴 데이터는 훈련 목적으로 사용할 수 없음  
+#   - 모델이 훈련 데이터를 전부 기억할 수 있기 때문  
+#
+#   
+# - 모델의 성능을 측정하려면 학습할 때 본 적 없는 데이터를 모델에 적용해야 함  
+#   -> 레이블된 데이터를 두 그룹으로 분리  
+#   - __훈련 데이터__ 혹은 __훈련 세트__ : 머신러닝 모델을 만들 때 사용  
+#   - __테스트 데이터__, __테스트 세트__, 혹은 __홀드아웃 세트__ : 모델이 얼마나 잘 작동하는지 측정하는 데 사용  
+#
+#   
+# - train_test_split : 데이터셋을 섞어서 나눠 줌  
+# - 전체 데이터 중 75%를 훈련 세트로, 25%를 테스트 세트로 뽑음  
+# - 데이터를 X로, 레이블을 y로 표기  
+# - 유사 난수 생성기에 넣을 난수 초깃값을 random_state 매개변수로 전달  
+#   -> 코드가 항상 같은 결과를 출력
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
@@ -50,20 +105,56 @@ print("y_train 크기:", y_train.shape)
 print("X_test 크기:", X_test.shape)
 print("y_test 크기:", y_test.shape)
 
+# ### 1.7.3 가장 먼저 할 일: 데이터 살펴보기
+# - 필요한 정보가 누락되지는 않았는지, 데이터에서 튀는 값이 있는지 찾아야 함  
+#
+#   
+# - 산점도 : 데이터를 시각화  
+#   - 산점도는 한 번에 3개 이상의 특성을 표현하기 어려움  
+#     -> 모든 특성을 짝지어 만드는 산점도 행렬을 사용
+
+# X_train 데이터를 사용해서 데이터프레임을 만듭니다.
+# 열의 이름은 iris_dataset.feature_names에 있는 문자열을 사용합니다.
 iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
+# 데이터프레임을 사용해 y_train에 따라 색으로 구분된 산점도 행렬을 만듭니다.
 pd.plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o', hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
+
+# ### 1.7.4 첫 번째 머신러닝 모델: k-최근접 이웃 알고리즘
+# - 데이터 포인트가 주어지면, 그 데이터 포인트에서 가장 가까운 훈련 데이터 포인트를 탐색  
+#
+#   
+# - k-최근접 이웃 알고리즘 : 데이터 포인트에서 가장 가까운 'k개'의 이웃을 찾음
+#   -> 빈도가 가장 높은 클래스를 예측값으로 사용  
+#
+#   
+# - n_neighbors : 이웃의 개수를 나타내는 매개변수
 
 from sklearn.neighbors import KNeighborsClassifier
 knn = KNeighborsClassifier(n_neighbors=1)
 
+# - 훈련 데이터셋으로부터 모델을 만들기 위해 knn 객체의 fit 메서드를 사용
+
 knn.fit(X_train, y_train)
+
+# ### 1.7.5 예측하기
+# - 만들어진 모델을 사용해서 정확한 레이블을 모르는 새 데이터에 대해 예측을 만들 수 있음  
+#   
+#   
+# - 새로운 테스트 데이터를 NumPy 배열로 생성
 
 X_new = np.array([[5, 2.9, 1, 0.2]])
 print("X_new.shape:", X_new.shape)
 
+# - scikit-learn은 항상 데이터가 2차원 배열일 것으로 예상
+
 prediction=knn.predict(X_new)
 print("예측:", prediction)
 print("예측한 타깃의 이름:", iris_dataset['target_names'][prediction])
+
+# ### 1.7.6 모델 평가하기
+# - 앞서 만든 테스트 세트를 사용
+# - 테스트 세트에 있는 각 붓꽃의 품종을 정확히 알고 있음
+# - 얼마나 많은 붓꽃 품종이 정확히 맞았는지 정확도를 계산하여 모델의 성능을 평가
 
 y_pred = knn.predict(X_test)
 print("테스트 세트에 대한 예측값:\n", y_pred)
@@ -71,3 +162,30 @@ print("테스트 세트에 대한 예측값:\n", y_pred)
 print("테스트 세트의 정확도: {:.2f}".format(np.mean(y_pred == y_test)))
 
 print("테스트 세트의 정확도: {:.2f}".format(knn.score(X_test, y_test)))
+
+# ## 1.8 요약 및 정리
+# - 지도 학습과 비지도 학습의 차이
+#   
+#   
+# 붓꽃의 품종이 무엇인지 예측하는 작업
+# - 정확한 품종으로 구분해 놓은 데이터셋을 사용했으므로 __지도 학습__에 해당  
+#   
+#   
+# - 품종이 세 개이므로 세 개의 클래스를 분류하는 문제  
+#   
+#   
+# - 각 품종을 __클래스__, 개별 붓꽃의 품종은 __레이블__이라고 함  
+#   
+#   
+# - 붓꽃 데이터셋은 두 개의 NumPy 배열 - X와 y - 로 이루어져 있음
+#   - X : 데이터. 특성들의 2차원 배열이므로 각 데이터 포인트는 행 하나로 나타남
+#   - y : 기대하는 출력. 1차원 배열로 각 샘플의 클래스 레이블에 해당하는 정수를 담고 있음
+#   
+#   
+# - 데이터셋을 모델 구축에 사용할 __훈련 세트__와 모델이 새로운 데이터에 얼마나 잘 적용될 수 있을지 평가하기 위한 __테스트 세트__로 나눔
+#   
+#   
+# - __k-최근접 이웃 분류__ 알고리즘은 새 데이터 포인트를 예측하기 위해 훈련 데이터에서 가장 가까운 이웃을 선택
+#   - n_neighbors 매개변수를 지정해 이 클래스의 객체를 생성
+#   - 훈련 데이터(X_train)와 훈련 데이터의 레이블(y_train)을 매개변수로 하여 fit 메서드를 호출해 모델 생성
+#   - 모델의 정확도를 계산하는 score 메서드로 모델을 평가
