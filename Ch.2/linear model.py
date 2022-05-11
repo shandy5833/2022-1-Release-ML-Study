c6860a6470225aa6465079119f402487108eb66d
shandy5833 <shandy5833@naver.com>
2022-05-11 18:01:50 +0900

    ~2.3.3 code only

--- Ch.2/linear model.py
+++ Ch.2/linear model.py
@@ -19,3 +19,225 @@ import pandas as pd
 import mglearn
 import warnings
 warnings.filterwarnings(action='ignore')
+
+mglearn.plots.plot_linear_regression_wave()
+
+# +
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+X, y = mglearn.datasets.make_wave(n_samples=60)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+lr = LinearRegression().fit(X_train, y_train)
+# -
+
+print("lr.coef_:", lr.coef_)
+print("lr.intercept_:", lr.intercept_)
+
+print("훈련 세트 점수: {:.2f}".format(lr.score(X_train, y_train)))
+print("테스트 세트 점수: {:.2f}".format(lr.score(X_test, y_test)))
+
+# +
+X, y = mglearn.datasets.load_extended_boston()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+lr = LinearRegression().fit(X_train, y_train)
+
+# +
+from sklearn.linear_model import Ridge
+
+ridge = Ridge().fit(X_train, y_train)
+print("훈련 세트 점수: {:.2f}".format(ridge.score(X_train, y_train)))
+print("테스트 세트 점수: {:.2f}".format(ridge.score(X_test, y_test)))
+# -
+
+ridge10 = Ridge(alpha=10).fit(X_train, y_train)
+print("훈련 세트 점수: {:.2f}".format(ridge10.score(X_train, y_train)))
+print("테스트 세트 점수: {:.2f}".format(ridge10.score(X_test, y_test)))
+
+ridge01 = Ridge(alpha=0.1).fit(X_train, y_train)
+print("훈련 세트 점수: {:.2f}".format(ridge01.score(X_train, y_train)))
+print("테스트 세트 점수: {:.2f}".format(ridge01.score(X_test, y_test)))
+
+# +
+plt.plot(ridge10.coef_, '^', label="Ridge alpha=10")
+plt.plot(ridge.coef_, 's', label="Ridge alpha=1")
+plt.plot(ridge01.coef_, 'v', label="Ridge alpha=0.1")
+
+plt.plot(lr.coef_, 'o', label="LinearRegression")
+plt.xlabel("계수 목록")
+plt.ylabel("계수 크기")
+xlims = plt.xlim()
+plt.hlines(0, xlims[0], xlims[1])
+plt.xlim(xlims)
+plt.ylim(-25, 25)
+plt.legend()
+# -
+
+mglearn.plots.plot_ridge_n_samples()
+
+# +
+from sklearn.linear_model import Lasso
+
+lasso = Lasso().fit(X_train, y_train)
+print("훈련 세트 점수: {:.2f}".format(lasso.score(X_train, y_train)))
+print("테스트 세트 점수: {:.2f}".format(lasso.score(X_test, y_test)))
+print("사용한 특성의 개수:", np.sum(lasso.coef_ != 0))
+# -
+
+# max_iter 기본값을 증가시키지 않으면 max_iter 값을 늘리라는 경고가 발생합니다
+lasso001 = Lasso(alpha=0.01, max_iter=50000).fit(X_train, y_train)
+print("훈련 세트 점수: {:.2f}".format(lasso001.score(X_train, y_train)))
+print("테스트 세트 점수: {:.2f}".format(lasso001.score(X_test, y_test)))
+print("사용한 특성의 개수:", np.sum(lasso001.coef_ != 0))
+
+lasso00001 = Lasso(alpha=0.0001, max_iter=50000).fit(X_train, y_train)
+print("훈련 세트 점수: {:.2f}".format(lasso00001.score(X_train, y_train)))
+print("테스트 세트 점수: {:.2f}".format(lasso00001.score(X_test, y_test)))
+print("사용한 특성의 개수:", np.sum(lasso00001.coef_ != 0))
+
+# +
+plt.plot(lasso.coef_, 's', label="Lasso alpha=1")
+plt.plot(lasso001.coef_, '^', label="Lasso alpha=0.01")
+plt.plot(lasso00001.coef_, 'v', label="Lasso alpha=0.0001")
+
+plt.plot(ridge01.coef_, 'o', label="Ridge alpha=0.1")
+plt.legend(ncol=2, loc=(0, 1.05))
+plt.ylim(-25, 25)
+plt.xlabel("계수 목록")
+plt.ylabel("계수 크기")
+
+# +
+from sklearn.linear_model import QuantileRegressor
+
+X, y = mglearn.datasets.make_wave(n_samples=60)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+pred_up = QuantileRegressor(quantile=0.9, alpha=0.01).fit(X_train, y_train).predict(X_test)
+pred_med = QuantileRegressor(quantile=0.5, alpha=0.01).fit(X_train, y_train).predict(X_test)
+pred_low = QuantileRegressor(quantile=0.1, alpha=0.01).fit(X_train, y_train).predict(X_test)
+
+plt.scatter(X_train, y_train, label='훈련 데이터')
+plt.scatter(X_test, y_test, label='테스트 데이터')
+plt.plot(X_test, pred_up, label='백분위:0.9')
+plt.plot(X_test, pred_med, label='백분위:0.5')
+plt.plot(X_test, pred_low, label='백분위:0.1')
+plt.legend()
+plt.show()
+
+# +
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+
+X, y = mglearn.datasets.make_forge()
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 3))
+
+for model, ax in zip([LinearSVC(max_iter=5000), LogisticRegression()], axes):
+    clf = model.fit(X, y)
+    mglearn.plots.plot_2d_separator(clf, X, fill=False, eps=0.5, ax=ax, alpha=.7)
+    mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+    ax.set_title(clf.__class__.__name__)
+    ax.set_xlabel("특성 0")
+    ax.set_ylabel("특성 1")
+axes[0].legend()
+# -
+
+mglearn.plots.plot_linear_svc_regularization()
+
+from sklearn.datasets import load_breast_cancer
+cancer = load_breast_cancer()
+X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, stratify=cancer.target, random_state=42)
+logreg = LogisticRegression(max_iter=5000).fit(X_train, y_train)
+print("훈련 세트 점수: {:.3f}".format(logreg.score(X_train, y_train)))
+print("테스트 세트 점수: {:.3f}".format(logreg.score(X_test, y_test)))
+
+logreg100 = LogisticRegression(C=100, max_iter=5000).fit(X_train, y_train)
+print("훈련 세트 점수: {:.3f}".format(logreg100.score(X_train, y_train)))
+print("테스트 세트 점수: {:.3f}".format(logreg100.score(X_test, y_test)))
+
+logreg001 = LogisticRegression(C=0.01, max_iter=5000).fit(X_train, y_train)
+print("훈련 세트 점수: {:.3f}".format(logreg001.score(X_train, y_train)))
+print("테스트 세트 점수: {:.3f}".format(logreg001.score(X_test, y_test)))
+
+plt.plot(logreg100.coef_.T, '^', label="C=100")
+plt.plot(logreg.coef_.T, 'o', label="C=1")
+plt.plot(logreg001.coef_.T, 'v', label="C=0.001")
+plt.xticks(range(cancer.data.shape[1]), cancer.feature_names, rotation=90)
+xlims = plt.xlim()
+plt.hlines(0, xlims[0], xlims[1])
+plt.xlim(xlims)
+plt.ylim(-5, 5)
+plt.xlabel("특성")
+plt.ylabel("계수 크기")
+plt.legend()
+
+# +
+from sklearn.datasets import make_blobs
+
+X, y = make_blobs(random_state=42)
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+plt.xlabel("특성 0")
+plt.ylabel("특성 1")
+plt.legend(["클래스 0", "클래스 1", "클래스 2"])
+# -
+
+linear_svm = LinearSVC().fit(X, y)
+print("계수 배열의 크기: ", linear_svm.coef_.shape)
+print("절편 배열의 크기: ", linear_svm.intercept_.shape)
+
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+line = np.linspace(-15, 15)
+for coef, intercept, color in zip(linear_svm.coef_, linear_svm.intercept_, mglearn.cm3.colors):
+    plt.plot(line, -(line * coef[0] + intercept) / coef[1], c=color)
+plt.ylim(-10, 15)
+plt.xlim(-10, 8)
+plt.xlabel("특성 0")
+plt.ylabel("특성 1")
+plt.legend(['클래스 0', '클래스 1', '클래스 2', '클래스 0 경계', '클래스 1 경계', '클래스 2 경계'], loc=(1.01, 0.3))
+
+mglearn.plots.plot_2d_classification(linear_svm, X, fill=True, alpha=.7)
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+line = np.linspace(-15, 15)
+for coef, intercept, color in zip(linear_svm.coef_, linear_svm.intercept_, mglearn.cm3.colors):
+    plt.plot(line, -(line * coef[0] + intercept) / coef[1], c=color)
+plt.legend(['클래스 0', '클래스 1', '클래스 2', '클래스 0 경계', '클래스 1 경계', '클래스 2 경계'], loc=(1.01, 0.3))
+plt.xlabel("특성 0")
+plt.ylabel("특성 1")
+
+# 한 줄에서 모델의 객체를 생성과 학습을 한번에 실행합니다.
+logreg = LogisticRegression().fit(X_train, y_train)
+
+logreg = LogisticRegression()
+y_pred = logreg.fit(X_train, y_train).predict(X_test)
+
+y_pred = LogisticRegression().fit(X_train, y_train).predict(X_test)
+
+# +
+from sklearn.linear_model import SGDClassifier
+
+sgd_c = SGDClassifier(alpha=0.01, learning_rate='adaptive', eta0=0.1, random_state=42, n_jobs=-1)
+sgd_c.fit(X, y)
+
+mglearn.plots.plot_2d_classification(sgd_c, X, fill=True, alpha=.7)
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+line = np.linspace(-15, 15)
+for coef, intercept, color in zip(sgd_c.coef_, sgd_c.intercept_, mglearn.cm3.colors):
+    plt.plot(line, -(line * coef[0] + intercept) / coef[1], c=color)
+plt.legend(['클래스 0', '클래스 1', '클래스 2', '클래스 0 경계', '클래스 1 경계', '클래스 2 경계'], loc=(1.01, 0.3))
+plt.xlabel("특성 0")
+plt.ylabel("특성 1")
+plt.show()
+
+# +
+from sklearn.linear_model import SGDRegressor
+
+X, y = mglearn.datasets.load_extended_boston()
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+sgd_r = SGDRegressor(learning_rate='adaptive', eta0=0.1, random_state=42)
+sgd_r.fit(X_train, y_train)
+
+print("훈련 세트 점수: {:.2f}".format(sgd_r.score(X_train, y_train)))
+print("테스트 세트 점수: {:.2f}".format(sgd_r.score(X_test, y_test)))
